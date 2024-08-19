0f6da4e0295347a8101511ee86771884495fbaf7
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-08-19 20:17:28 +0530

    commit

---
+++ 02-Node/file.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+//can use the fs module to interact with files
+
+//synchronous call
+// fs.writeFileSync("./text.txt", "Hello World");
+
+//async call
+// fs.writeFile("./text.txt", "Hello World Async", (err) => {});
+
+//To read the contacts.txt file
+// const result = fs.readFileSync("./contacts.txt", "utf-8");
+// console.log(result);
+
+//async doesnt return anything and always expects a callback function
+fs.readFile("contacts.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log("There is an error");
+  } else {
+    console.log(result);
+  }
+});
