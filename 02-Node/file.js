8562b6649d76d94bf27880d280709f73f8e06e20
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-08-21 20:40:19 +0530

    commit

--- 02-Node/file.js
+++ 02-Node/file.js
@@ -1,25 +1,34 @@
 const fs = require("fs");
+const os = require("os");
 
-//can use the fs module to interact with files
+//gives you an ouput 8, that means the maximum thread size is 8
+console.log(os.cpus().length);
 
-//synchronous call
-// fs.writeFileSync("./text.txt", "Hello World");
+// can use the fs module to interact with files
 
-//async call
-// fs.writeFile("./text.txt", "Hello World Async", (err) => {});
+// synchronous call(Blocing req)
+fs.writeFileSync("./text.txt", "Hello World");
 
-//To read the contacts.txt file
-// const result = fs.readFileSync("./contacts.txt", "utf-8");
-// console.log(result);
+// async call(Non-blocking req)
+fs.writeFile("./text.txt", "Hello World Async", (err) => {});
 
-//async doesnt return anything and always expects a callback function
-// fs.readFile("contacts.txt", "utf-8", (err, result) => {
-//   if (err) {
-//     console.log("There is an error");
-//   } else {
-//     console.log(result);
-//   }
-// });
+// To read the contacts.txt file
+const result = fs.readFileSync("./contacts.txt", "utf-8");
+console.log(result);
 
-//to append things in the file
+// async doesnt return anything and always expects a callback function
+fs.readFile("contacts.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log("There is an error");
+  } else {
+    console.log(result);
+  }
+});
+
+// to append things in the file
 fs.appendFileSync("./text.txt", `${Date.now()}Hey there\n`);
+
+// default thread pool size
+// 8core cpu
+// You cant make 9 threads because of the default thread pool size, so you can use the os module.
+// os gives you information about your own computer
