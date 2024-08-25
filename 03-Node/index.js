e16c8da161e6c68170f249db8320e34bba65e0af
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-08-25 18:01:03 +0530

    commit

--- 03-Node/index.js
+++ 03-Node/index.js
@@ -3,11 +3,14 @@
 //to make a http server, we have a built in mpdule called http
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 //this function takes a callback function which can process the incoming request
 const myServer = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()} : ${req.url} New request recieved\n `;
+  const myUrl = url.parse(req.url);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
     switch (req.url) {
       case "/":
