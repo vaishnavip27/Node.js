b9c31292710dc0a5d7438889adde9ddb09cbe756
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-08-25 19:44:39 +0530

    commit

--- 03-Node/index.js
+++ 03-Node/index.js
@@ -8,17 +8,25 @@ const url = require("url");
 //this function takes a callback function which can process the incoming request
 const myServer = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()} : ${req.url} New request recieved\n `;
+  const log = `${Date.now()} : ${req.method} ${
+    req.url
+  } New request recieved\n `;
   const myUrl = url.parse(req.url);
   console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
     switch (req.url) {
       case "/":
-        res.end("Homepage");
+        if (req.method === "GET") res.end("Homepage");
         break;
       case "/about":
         res.end("About Page");
         break;
+      case "/Signup":
+        if (req.method === "GET") res.end("This is a signup page");
+        else if (req.method === "POST") {
+          //DB query
+          res.end("Success");
+        }
       default:
         res.end("404 not found");
     }
