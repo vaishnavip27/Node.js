e9524db25c78727cf1edf957006bfc810b50eeea
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-09-05 10:01:57 +0530

    commit

--- 04-Node/index.js
+++ 04-Node/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 
 const app = express();
 
 const PORT = 8000;
+
+//Middleware
+app.use(express.urlencoded({ extended: false }));
 
 //define routes
 app.get("/users", (req, res) => {
@@ -37,7 +41,11 @@ app
 
 app.post("/api/users", (req, res) => {
   //Todo: Create new user
-  return res.json({ status: "pending" });
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "success", id: users.length });
+  });
 });
 
 app.listen(PORT, () => console.log(`Server started : ${PORT}`));
