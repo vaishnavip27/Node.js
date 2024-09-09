e198df34e8c7d7836a1b913f76d12f17272e1d0a
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-09-09 23:12:49 +0530

    commit

--- 04-Node/controllers/user.js
+++ 04-Node/controllers/user.js
@@ -1,1 +1,60 @@
 // we have functions in the controller which we attach with the routesw
+const User = require("../models/user");
+
+async function handleGetAllUsers(req, res) {
+  const allDbUsers = await User.find({});
+  return res.json(allDbUsers);
+}
+
+async function handleGetUserById(req, res) {
+  const user = await User.findById(req.params.id);
+  // const id = Number(req.params.id);
+
+  //finding the id in the json file
+  // const user = users.find((user) => user.id === id);
+  if (!user) return res.status(404).json({ error: "User not found" });
+  return res.json(user);
+}
+
+async function handleUpdateUserById(req, res) {
+  await User.findByIdAndUpdate(req.params.id, { lastName: "Changed" });
+  return res.json({ status: "Success" });
+}
+
+async function handleDeleteUserById(req, res) {
+  await User.findByIdAndUpdate(req.params.id, { lastName: "Changed" });
+  return res.json({ status: "Success" });
+}
+
+async function handleCreateNewUserUser(req, res) {
+  //Todo: Create new user
+  const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({ msg: "All field are required" });
+  }
+
+  const result = await User.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.job_title,
+  });
+
+  return res.status(201).json({ msg: "Success", id: result._id });
+}
+
+model.exports = {
+  handleGetAllUsers,
+  handleGetUserById,
+  handleUpdateUserById,
+  handleDeleteUserById,
+  handleCreateNewUserUser,
+};
--- 04-Node/routes/user.js
+++ 04-Node/routes/user.js
@@ -1,61 +1,32 @@
 const express = require("express");
+const {
+  handleGetAllUsers,
+  handleGetUserById,
+  handleUpdateUserById,
+  handleDeleteUserById,
+  handleCreateNewUserById,
+} = require("../controllers/user");
 
 const router = express.Router();
 
 //rest api points
-router.get("/", async (req, res) => {
-  const allDbUsers = await User.find({});
-  return res.json(allDbUsers);
-  // res.setHeader("X-MyName", "Vaishnavi Patil"); //custom header
-  //Always add X to custom headers
-});
+//returning me all the users
+// router.get("/", async (req, res) => {
+//   const allDbUsers = await User.find({});
+//   return res.json(allDbUsers);
+// res.setHeader("X-MyName", "Vaishnavi Patil"); //custom header
+//Always add X to custom headers
+// });
+
+router.get("/", handleGetAllUsers);
 
 router
   .route("/:id")
   //the below are the controllers
-  .get(async (req, res) => {
-    const user = await User.findById(req.params.id);
-    // const id = Number(req.params.id);
+  .get(handleGetUserById)
+  .patch(handleUpdateUserById)
+  .delete(handleDeleteUserById);
 
-    //finding the id in the json file
-    // const user = users.find((user) => user.id === id);
-    if (!user) return res.status(404).json({ error: "User not found" });
-    return res.json(user);
-  })
-  .patch(async (req, res) => {
-    //Edit user with Id
-    await User.findByIdAndUpdate(req.params.id, { lastName: "Changed" });
-    return res.json({ status: "Success" });
-  })
-  .delete(async (req, res) => {
-    //Todo: Delete the user with
-    await User.findByIdAndDelete(req.params.id);
-    return res.json({ status: "Success" });
-  });
-
-router.post("/", async (req, res) => {
-  //Todo: Create new user
-  const body = req.body;
-  if (
-    !body ||
-    !body.first_name ||
-    !body.last_name ||
-    !body.email ||
-    !body.gender ||
-    !body.job_title
-  ) {
-    return res.status(400).json({ msg: "All field are required" });
-  }
-
-  const result = await User.create({
-    firstName: body.first_name,
-    lastName: body.last_name,
-    email: body.email,
-    gender: body.gender,
-    jobTitle: body.job_title,
-  });
-
-  return res.status(201).json({ msg: "Success" });
-});
+router.post("/", handleCreateNewUserById);
 
 module.exports = router;
