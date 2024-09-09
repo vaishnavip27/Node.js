f0dad29ea4df42e1d65064c4bbc0053df852b618
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-09-09 12:04:30 +0530

    commit

--- 04-Node/index.js
+++ 04-Node/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 
+const { logReqRes } = require("./middlewares");
 const { connectMongoDb } = require("./connection");
 const userRouter = require("./routes/user");
 
@@ -14,17 +15,8 @@ connectMongoDb("mongodb://127.0.0.1:27017/youtube-app-1");
 //Middleware
 app.use(express.urlencoded({ extended: false }));
 
-//Building a middleware handler function
-app.use((req, res, next) => {
-  fs.appendFile(
-    "log.txt",
-    `\n${Date.now()} : ${req.ip}: ${req.method} : ${req.path}`,
-    (err, data) => {
-      next();
-    }
-  );
-});
-
+//middlewares
+app.use(logReqRes("log.txt"));
 //routes
 app.use("/user", userRouter);
 
