16f84f96119dd771f35f978253ebc8f65ea320fd
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-09-13 02:49:52 +0530

    commit

--- 05-Node/index.js
+++ 05-Node/index.js
@@ -1,8 +1,35 @@
 const express = require("express");
-
+const urlRoute = require("./routes/url");
+const { connectToMongoDB } = require("./connect");
+const URl = require("./models/url");
 const app = express();
 
 const PORT = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() => {
+  console.log("MONGODb connected ");
+});
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitedHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
 
 app.listen(PORT, () => {
   console.log(`Server started at PORT: ${PORT}`);
--- 05-Node/routes/url.js
+++ 05-Node/routes/url.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const { handleGenerateNewShortURL } = require("../controllers/url");
 const router = express.Router();
 
-router.post("/");
+router.post("/", handleGenerateNewShortURL);
+
+module.exports = router;
